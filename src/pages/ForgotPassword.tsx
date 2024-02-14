7b8038cbe9dfd4d427cbd771c5f22a9765ebd5d0
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-02-14 21:09:53 +0700

    refactor(seprate-validate-file)

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -3,6 +3,7 @@ import { axiosInstance } from "@/utils/Axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { MySwal } from "@/utils/Swal";
+import { checkPassword, checkEmail } from "@/utils/validate";
 
 const ForgotPassword = () => {
   const navigate = useNavigate();
@@ -16,18 +17,6 @@ const ForgotPassword = () => {
   const [errEmail, setErrEmail] = useState(true);
   const [errPassword, setErrPassword] = useState(true);
   // const [errOtp, setErrOtp] = useState(true);
-
-  const checkPassword = (value: string) => {
-    const regEx =
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    return regEx.test(value);
-  };
-
-  const checkEmail = () => {
-    const regEx =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regEx.test(email);
-  };
 
   // const checkOtp = () => {
   //   const regEx = new RegExp("^[0-9]+$");
@@ -111,7 +100,7 @@ const ForgotPassword = () => {
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-start mt-4 px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full p-6 bg-white rounded-lg shadow border md:mt-0 sm:max-w-md sm:p-8">
           <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
             Forgot Password
@@ -132,7 +121,7 @@ const ForgotPassword = () => {
                     id="email"
                     onChange={(e) => {
                       setEmail(e.target.value);
-                      setErrEmail(checkEmail());
+                      setErrEmail(checkEmail(email));
                     }}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 disabled:bg-slate-200"
                     placeholder="test@mail.com"
@@ -232,7 +221,6 @@ const ForgotPassword = () => {
                         onChange={(e) => {
                           setReNewPassword(e.target.value);
                           setErrPassword(checkPassword(renewPassword));
-                          console.log(errPassword);
                         }}
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 disabled:bg-slate-200"
                         disabled={!isOTPVefified || loading}
---
+++ src/utils/validate.ts
@@ -0,0 +1,10 @@
+export const checkPassword = (value: string) => {
+    const regEx =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    return regEx.test(value);
+  };
+export const checkEmail = (value : string) => {
+  const regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regEx.test(value);
+};
