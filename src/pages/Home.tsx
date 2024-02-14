62d23246876344ded110fa2467ec05973a67a9ab
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-02-15 00:20:23 +0700

    wip(homepage)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 const Home = () => {
   return (
-    <div className="text-center">
-      <h2 className="text-lg font-medium"> this is home page</h2>
-      <h1 className="text-3xl text-blue-500 font-bold">
-        React(TS) + Nest.js Authentication
-      </h1>
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <div className="text-center">
+          <h1 className="text-3xl text-blue-500 font-bold">
+            React(TS) + Nest.js Authentication
+          </h1>
+          <h2 className="text-lg font-medium"> This is home page</h2>
+        </div>
+      </div>
     </div>
   );
 };
